a481f2871661480c71cc3be2a91c5c7a199d857e
Heba Hossam <95322482+HebaHossam68@users.noreply.github.com>
2023-10-13 00:26:34 +0300

    Task1_ point1.py

--- Point1/point1.py
+++ Point1/point1.py
@@ -1,12 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Define the file path
+
 file_path = "C:/Users/future/Downloads/signal1.txt"
 
 
 with open(file_path, "r") as file:
-    # Skip the first three lines of metadata
     for _ in range(3):
         next(file)
 
@@ -41,13 +40,8 @@ values = np.array(values)
 #plt.show()
 
 plt.figure(figsize=(10, 6))
-
-# Plot the continuous representation
 plt.plot(indices, values, marker='o', linestyle='-', label="Continuous")
-
-# Overlay the discrete representation using stem
 plt.stem(indices, values, basefmt=" ", use_line_collection=True, label="Discrete")
-
 plt.title("Combined Continuous and Discrete Representation")
 plt.xlabel("Sample Index (n)")
 plt.ylabel("Value")
