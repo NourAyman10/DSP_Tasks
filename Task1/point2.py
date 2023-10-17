0b9a3816dab98ae323f7b78b4e366a5bff2cb823
Nourhan Ayman <83882344+Nour10102002@users.noreply.github.com>
2023-10-18 00:02:14 +0300

    handel inputs

--- Task1/point2.py
+++ Task1/point2.py
@@ -7,6 +7,7 @@ from comparesignals import SignalSamplesAreEqual
 from tkinter import messagebox
 
 
+
 class SecondPoint:
     def __init__(self):
         self.mainColor = '#270D30'
@@ -19,18 +20,27 @@ class SecondPoint:
         self.root.geometry("1000x563")
 
         # setting background image
+        self.setting_background()
+
+        # Creating widgets
+        self.create_widgets()
+
+        # Placing widgets on the screen
+        self.placing_widgets()
+
+        self.root.mainloop()
+
+    def setting_background(self):
         self.image = Image.open("Photos/Point2/background2.png")
         self.background2_image = ImageTk.PhotoImage(self.image)
         self.background2_label = Label(self.root, image=self.background2_image)
-
         self.main_frame2 = tk.Frame(self.root, borderwidth=0, background=self.mainColor)
         self.image_label = tk.Label(self.main_frame2, image="", borderwidth=0, background=self.mainColor)
 
-        # Creating widgets
+    def create_widgets(self):
         self.choose_type = Image.open("Photos/chooseType.png")
         self.choose_type_image = ImageTk.PhotoImage(self.choose_type)
         self.choose_type_label = Label(self.root, image=self.choose_type_image, background=self.mainColor)
-
         # Type(sin, cos) Radio Button
         self.type_value = StringVar(value="none")
         self.SinImage = PhotoImage(file="Photos/Point2/sinusoidal.png")
@@ -41,43 +51,36 @@ class SecondPoint:
         self.CosRadioButton = Radiobutton(self.root, variable=self.type_value,
                                           value="cos", background=self.mainColor, image=self.CosImage,
                                           activebackground=self.mainColor)
-
         # Textbox inputs labels
         self.inputs_labels = Image.open("Photos/Point2/inputs.png")
         self.inputs_labels_image = ImageTk.PhotoImage(self.inputs_labels)
         self.inputs_labels_label = Label(self.root, image=self.inputs_labels_image, background='#18103A')
-
         self.amplitude_value = StringVar(value="")
         self.amplitudeEntry = Entry(self.root, width=18, font=("arial", 15), bd=0, textvariable=self.amplitude_value,
                                     background=self.secondColor,
                                     foreground=self.foregroundColor, insertbackground="#9601AB")
-
         self.analogFrequency_value = StringVar(value="")
         self.analogFrequencyEntry = Entry(self.root, width=18, font=("arial", 15), bd=0,
                                           textvariable=self.analogFrequency_value,
                                           background=self.secondColor,
                                           foreground=self.foregroundColor, insertbackground="#9601AB")
-
         self.samplingFrequency_value = StringVar(value="")
         self.samplingFrequencyEntry = Entry(self.root, width=18, font=("arial", 15), bd=0,
                                             textvariable=self.samplingFrequency_value,
                                             background=self.secondColor,
                                             foreground=self.foregroundColor, insertbackground="#9601AB")
-
         self.phaseShift_value = StringVar(value="")
         self.phaseShiftEntry = Entry(self.root, width=18, font=("arial", 15), bd=0, textvariable=self.phaseShift_value,
                                      background=self.secondColor,
                                      foreground=self.foregroundColor, insertbackground="#9601AB")
-
         self.generate_button_image = PhotoImage(file="Photos/Point2/generateBtn.png")
         self.generateButton = Button(self.root, image=self.generate_button_image, borderwidth=0, cursor="hand2", bd=0,
                                      background='#141345', activebackground='#141345', command=self.generate_wave)
-
         self.compare_button_image = PhotoImage(file="Photos/Point2/compareBtn.png")
         self.compareButton = Button(self.root, image=self.compare_button_image, borderwidth=0, cursor="hand2", bd=0,
                                     background='#141345', activebackground='#141345', command=self.compare_values)
 
-        # Placing widgets on the screen
+    def placing_widgets(self):
         self.background2_label.place(x=0, y=0)
         self.main_frame2.place(anchor='center', relx=0.5, rely=0.45)
         self.choose_type_label.place(anchor='center', relx=0.5, y=130)
@@ -90,36 +93,47 @@ class SecondPoint:
         self.phaseShiftEntry.place(anchor='center', relx=0.73, y=360)
         self.generateButton.place(anchor='center', relx=0.35, y=430)
         self.compareButton.place(anchor='center', relx=0.65, y=430)
-
         self.image_label.pack()
 
-        self.root.mainloop()
-
     def generate_wave(self):
-        amplitude = float(self.amplitude_value.get())
-        analog_frequency = float(self.analogFrequency_value.get())
-        sampling_frequency = float(self.samplingFrequency_value.get())
-        phase_shift = float(self.phaseShift_value.get())
+        # validate inputs
+        validate = bool(self.validate_inputs())
 
-        # sampling frequency obeys the sampling theorem
-        min_sampling_frequency = 2 * analog_frequency
-        if sampling_frequency < min_sampling_frequency:
-            messagebox.showerror("Error!!", f"Sampling frequency should be at least {min_sampling_frequency} Hz.")
-        else:
-            self.time = np.arange(0, 1, 1 / sampling_frequency)
-            if self.type_value.get() == "sin":
-                self.Y = amplitude * np.sin((2 * np.pi * analog_frequency * self.time) + phase_shift)
-            elif self.type_value.get() == "cos":
-                self.Y = amplitude * np.cos((2 * np.pi * analog_frequency * self.time) + phase_shift)
+        if validate:
+            amplitude = float(self.amplitude_value.get())
+            analog_frequency = float(self.analogFrequency_value.get())
+            sampling_frequency = float(self.samplingFrequency_value.get())
+            phase_shift = float(self.phaseShift_value.get())
+
+            # sampling frequency obeys the sampling theorem
+            min_sampling_frequency = 2 * analog_frequency
+            if sampling_frequency < min_sampling_frequency:
+                messagebox.showerror("Error!!", f"Sampling frequency should be at least {min_sampling_frequency} Hz.")
             else:
-                messagebox.showerror("Error!!", "Please, Enter valid type")
+                self.time = np.arange(0, 1, 1 / sampling_frequency)
+                if self.type_value.get() == "sin":
+                    self.Y = amplitude * np.sin((2 * np.pi * analog_frequency * self.time) + phase_shift)
+                elif self.type_value.get() == "cos":
+                    self.Y = amplitude * np.cos((2 * np.pi * analog_frequency * self.time) + phase_shift)
+                else:
+                    messagebox.showerror("Error!!", "Please, Enter valid type")
 
-            plt.figure(figsize=(20, 5))
-            plt.plot(self.time, self.Y)
-            plt.title(f"{(self.type_value.get()).capitalize()} Wave")
-            plt.xlabel("Time(s)")
-            plt.ylabel("Amplitude")
-            plt.show()
+                self.draw_wave()
+
+    def draw_wave(self):
+        plt.figure(figsize=(20, 5))
+        plt.plot(self.time, self.Y)
+        plt.title(f"{(self.type_value.get()).capitalize()} Wave")
+        plt.xlabel("Time(s)")
+        plt.ylabel("Amplitude")
+        plt.show()
+
+    def validate_inputs(self):
+        if self.type_value.get().strip() and str(self.amplitude_value.get()).strip() and str(self.analogFrequency_value.get()).strip() and str(self.samplingFrequency_value.get()).strip() and str(self.phaseShift_value.get()).strip():
+            return True
+        else:
+            messagebox.showerror("Error!!", "Please, Fill all empty inputs")
+            return False
 
     def compare_values(self):
         if self.type_value.get() == "sin":
